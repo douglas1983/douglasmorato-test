3f27aa05add07c828352779dd7d5113c022a80df
Douglas Morato <dmoratos@gmail.com>
2021-09-15 18:00:36 -0300

    mongoconfig

--- src/api/config/mongoconfig.js
+++ src/api/config/mongoconfig.js
@@ -1,10 +1,10 @@
 const { MongoClient } = require('mongodb');
 
-const url = process.env.MONGO_DB_URL;
+const url = process.env.MONGO_DB_URL || 'mongodb://localhost:27017/Cookmaster';
 const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Database Name
-const dbName = process.env.DB_NAME;
+const dbName = process.env.DB_NAME || 'Cookmaster';
 const collection = async (col) => {
   // eslint-disable-next-line no-underscore-dangle
   if (client._eventsCount === 0) {
